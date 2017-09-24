b7a5b07bd0e4cc06eb2f8c3e0992928de2b2828d
Anastasiya Shtunder <fresh.vns16@outlook.com>
2017-09-24 20:04:18 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,8 @@ import { DBLoggedIn, DBReady } from './db';
 import { MeComponent } from './me/me.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/signup', pathMatch: 'full', resolve: { db: DBReady }}, // redirect to signup page
+  { path: '', redirectTo: '/signup' }, // redirect to signup page
   { path: 'signup', component: SignupComponent, resolve: { db: DBReady } }, // will activate the route after the db is ready
-  { path: 'signup/me', component: MeComponent, canActivate: [DBLoggedIn] }, // will prevent none logged in users from accessing it
   { path: 'chats', loadChildren: './chats/chats.module#ChatsModule'} // Using loadChildren to async load the route
 ];
 
