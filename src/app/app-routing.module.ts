4f7f1206a41516a871bd5d1728fa88f9c2894f98
Anastasiya Shtunder <fresh.vns16@outlook.com>
2017-09-25 17:16:31 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LenderComponent } from './home/lender.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to home page
   { path: 'home', component: HomeComponent },
-  { path: 'lender-select', component: HomeComponent },
+  { path: 'lender-select', component: LenderComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LenderComponent } from './home/lender.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LenderComponent
   ],
   imports: [
     BrowserModule,
